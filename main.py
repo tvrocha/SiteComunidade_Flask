6cc450aa18d9eb9da83b042470a4530673ef01de
tvrocha <tuliovcr@outlook.com>
2024-10-28 22:37:18 -0300

    formulario feito e mensagem de success

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormCriarConta, FormLogin
 
 app = Flask(__name__)
@@ -23,10 +23,16 @@ def pagina_usuarios():
     return render_template("usuarios.html", lista_usuarios=lista_usuarios)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def pagina_login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
+    if form_login.validate_on_submit() and 'button_submit_login' in request.form:
+        flash(f'Login feito com sucesso para email: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    if form_criarconta.validate_on_submit() and 'button_submit_criarconta' in request.form:
+        flash(f'Conta criada com sucesso para email: {form_criarconta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
     return render_template("login.html", form_login=form_login, form_criarconta=form_criarconta)
 
 
